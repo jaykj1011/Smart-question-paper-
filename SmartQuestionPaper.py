6af324523f256c65269cf2780e7ffd40fc44da5c
jaykj1011 <143266799+jaykj1011@users.noreply.github.com>
2024-05-19 17:10:02 +0530

    Update SmartQuestionPaper.py

--- SmartQuestionPaper.py
+++ SmartQuestionPaper.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread('Python/contours.png')
+img = cv2.imread('Python/contours.png')            
 img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
